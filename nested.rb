dd99284591ac66786611c106a0827bab3f5c4467
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-11 01:38:06 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = "C"
+programmer_hash[:dennis_ritchie][:languages]
+
+
 end
 
 def adding_matz
@@ -79,7 +84,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
     
 end
 
@@ -101,6 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
      
      
 end
@@ -122,4 +131,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
